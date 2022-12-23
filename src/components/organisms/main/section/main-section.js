71b107bfb931b1cbe07fffaac1c597eb7d2d9fbf
Pavel Emelyanov <lictratenkodenis26@gmail.com>
2022-12-23 19:22:09 +0300

    scss change

--- src/components/organisms/main/section/main-section.js
+++ src/components/organisms/main/section/main-section.js
@@ -7,7 +7,9 @@ export class MainSection extends Component{
     render(){
         return`
         <section class="section">
-            <img src="../../../../assets/images/main/1.jpg" alt=""eco-house" class="eco-house">
+            <p class="eco-house-p">
+                <img src="../../../../assets/images/main/1.jpg" alt=""eco-house" class="eco-house">
+            </p>
             <main-partners class="partners"></main-partners>
             <hr>
             <personal-data-work></personal-data-work>
@@ -17,17 +19,3 @@ export class MainSection extends Component{
 }
 customElements.define('main-section',MainSection)
 
-{/* <section class="section">
-            <img src="../../../../assets/images/main/1.jpg" alt=""eco-house" class="eco-house">
-            <div class="partners">
-                <a href=""><img src="../../../../assets/images/main/Банер-Ельск.png" alt="Банер Ельск"></a>
-                <a href=""><img src="../../../../assets/images/main/byspan.png" alt="BySpan"></a>
-                <a href=""><img src="../../../../assets/images/main/BFC.png" alt="BFC"></a>
-            </div>
-            <hr>
-            <div class="politic">
-                <p>
-                    <a href="./pdf/main/638_02.12.2021.pdf">Политика обработки персональных данных</a>
-                </p>
-            </div>
-        </section> */}
